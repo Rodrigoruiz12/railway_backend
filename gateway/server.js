0ce79689175cd0f1a43f2a8f854a2cc9b4763d75
Rodrigoruiz12 <rod.ruiz@duocuc.cl>
2025-11-26 19:07:04 -0300

    cambios server.js de gateway

--- gateway/server.js
+++ gateway/server.js
@@ -6,13 +6,26 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Redirigimos cada petición al microservicio correcto
-app.use('/api/products', proxy('http://localhost:3001')); // Al servicio de Productos
-app.use('/api/login', proxy('http://localhost:3002'));    // Al servicio de Login
-app.use('/api/users', proxy('http://localhost:3003'));    // Al servicio de Users (Admin)
-app.use('/api/cart', proxy('http://localhost:3004'));     // Al servicio de Carrito
-app.use('/api/blog', proxy('http://localhost:3005'));     // Al servicio de Blog
+// Definimos las URLs: Primero intentamos leer la variable de entorno, si no existe, usamos localhost
+const PRODUCT_URL = process.env.PRODUCT_SERVICE_URL || 'http://localhost:3001';
+const LOGIN_URL   = process.env.LOGIN_SERVICE_URL   || 'http://localhost:3002';
+const USER_URL    = process.env.USER_SERVICE_URL    || 'http://localhost:3003';
+const CART_URL    = process.env.CART_SERVICE_URL    || 'http://localhost:3004';
+const BLOG_URL    = process.env.BLOG_SERVICE_URL    || 'http://localhost:3005';
 
-app.listen(3000, () => {
-    console.log('API Gateway corriendo en http://localhost:3000');
+console.log(`Configurando rutas...`);
+console.log(`Products -> ${PRODUCT_URL}`);
+console.log(`Login    -> ${LOGIN_URL}`);
+
+// Redirigimos cada petición al microservicio correcto usando las variables
+app.use('/api/products', proxy(PRODUCT_URL));
+app.use('/api/login',    proxy(LOGIN_URL));
+app.use('/api/users',    proxy(USER_URL));
+app.use('/api/cart',     proxy(CART_URL));
+app.use('/api/blog',     proxy(BLOG_URL));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`API Gateway corriendo en puerto ${PORT}`);
 });
